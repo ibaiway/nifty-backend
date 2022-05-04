08b6463d50a36eb87c9113fa8c11ea88824c3fad
ibaialberdi <ibaialberdi@gmail.com>
2022-05-04 11:21:55 +0200

    Created Playlist Model

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,11 @@
 import UserModel from './user-model.js';
 import TrackModel from './track-model.js';
 import GenreModel from './genre-model.js';
+import PLaylistModel from './playlist-model.js';
 
 export default {
   User: UserModel,
   Track: TrackModel,
-  Genre: GenreModel
+  Genre: GenreModel,
+  Playlist: PLaylistModel
 };
