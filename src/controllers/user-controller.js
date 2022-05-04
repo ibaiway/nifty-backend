4e319b2c7ae7d61ffbde43ddae2d8c42c31b8309
Albert Garcia Fernandez <alberd_beto@hotmail.com>
2022-05-04 15:51:44 +0200

    more user controllers add

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -67,7 +67,9 @@ async function updateUser(req, res, next) {
     ).select({
       email: 1,
       firstName: 1,
-      lastName: 1
+      lastName: 1,
+      profileImage: 1,
+      artist: 1
     });
 
     res.status(200).send({
