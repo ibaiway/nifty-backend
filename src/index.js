fd6122754c4f39d284b14eeba9b8a73e2216303d
ibaialberdi <ibaialberdi@gmail.com>
2022-05-19 17:28:05 +0200

    Cron job schedul working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import cron from 'node-cron';
 import app from './server.js';
 import config from './config/config.js';
-import connect from "./db/connect.js";
+import connect from './db/connect.js';
+import callStats from './controllers/stats-controller.js';
 
 connect().then(async function onServerInit() {
   config.logger.info(`Mongo DB connected`);
@@ -8,4 +10,15 @@ connect().then(async function onServerInit() {
   app.listen(config.app.PORT, () => {
     config.logger.info(`Server running at http://localhost:${config.app.PORT}`);
   });
+
+  cron.schedule(
+    '*/15 * * * *',
+    () => {
+      callStats();
+    },
+    {
+      scheduled: true,
+      timezone: 'Europe/Madrid'
+    }
+  );
 });
